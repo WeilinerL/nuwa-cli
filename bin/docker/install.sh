17d76af641c7b4e8ee0bc351452ad264b80e54fb
WeilinerL <2207284210@qq.com>
2022-01-16 13:36:35 +0800

    chore: 调整构建顺序

--- bin/docker/install.sh
+++ bin/docker/install.sh
@@ -3,9 +3,7 @@
 echo "开始下载docker..."
 
 # 使用国内 daocloud 一键安装命令
-curl -sSL https://get.daocloud.io/docker | sh
-
-echo "安装完成"
-
-# 启动docker
-systemctl start docker
+curl -sSL https://get.daocloud.io/docker | sh \
+  && echo "安装完成" \
+  # 启动docker
+  && systemctl start docker
--- bin/docker/start.sh
+++ bin/docker/start.sh
@@ -4,20 +4,23 @@ sudo chmod +x ./install.sh
 sudo chmod +x ./local-registry.sh
 
 # 安装docker
-./install.sh
+./install.sh \
+  # 安装启动本地镜像仓库
+  && ./local-registry.sh \
+  # 构建基础镜像
+  && docker build -t 127.0.0.1:5000/nuwa-base:latest -f Dockerfile.base . \
+  # 推到本地仓库
+  && docker push 127.0.0.1:5000/nuwa-base:latest \
+  # 构建项目镜像
+  && docker build -t 127.0.0.1:5000/nuwa:latest . \
+  # 启动项目
+  # 将镜像仓库（容器）的5951端口映射到服务器的5951端口
+  && docker run -d -p 5951:5951 127.0.0.1:5000/nuwa:latest
 
-# 安装启动本地镜像仓库
-./local-registry.sh
 
-# 构建基础镜像
-docker build -t 127.0.0.1:5000/nuwa-base:latest -f Dockerfile.base .
 
-# 推到本地仓库
-docker push 127.0.0.1:5000/nuwa-base:latest
 
-# 构建项目镜像
-docker build -t 127.0.0.1:5000/nuwa:latest .
 
-# 启动项目
-# 将镜像仓库（容器）的5951端口映射到服务器的5951端口
-docker run -d -p 5951:5951 127.0.0.1:5000/nuwa:latest
+
+
+
